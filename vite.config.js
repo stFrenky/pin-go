55165d88c4709309afab01ca9d579e0b1af45073
stfrenky <shoma.albakov69@gmail.com>
2023-06-04 21:40:26 +0300

    update: ci/cd

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig({
                 ...INPUTS
             },
         },
+        base: '/pin-go/',
         css: {
             preprocessorOptions: {
                 scss: {
